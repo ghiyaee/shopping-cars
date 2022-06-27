773ff70d392cac89a208116426abb44be2a6a17e
ghiyaee <86221396+ghiyaee@users.noreply.github.com>
2022-06-28 00:53:00 +0430

    add logo

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,10 +3,11 @@ const Header = ({setShow,basket}) => {
     return (
       <nav className="nav__container">
         <div className="nav__box">
-          <div className="nav__logo" onClick={()=> setShow(true)}>
+          <div className="nav__logo" onClick={() => setShow(true)}>
+            <i className="fa-solid fa-store icon"></i>
             Shopping Cars
           </div>
-          <div className="nav__basket" onClick={()=>setShow(false)}>
+          <div className="nav__basket" onClick={() => setShow(false)}>
             <span>
               <i className="fa-solid fa-cart-arrow-down"></i>
             </span>
