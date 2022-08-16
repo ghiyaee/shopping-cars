a334385fbbd663994fd72a642e1f10034270da6f
ghiyaee <86221396+ghiyaee@users.noreply.github.com>
2022-08-16 17:34:41 +0430

    add date

--- src/data.js
+++ src/data.js
@@ -1,112 +1,138 @@
-const data = [
-     {
-         id: 1,
-         brand: 'BENZ',
-         model: 2022,
-         price: 50000,
-         color: 'black',
+const data = [{
+        id: 1,
+        brand: 'BENZ',
+        model: 2022,
+        price: 50000,
+        color: 'black',
         img: 'https://cdn.motor1.com/images/mgl/Oovj9l/s1/2022-mercedes-amg-gt-63-63-s-4-door-coupe.jpg',
-         conter:1
-    },
-        {
-            id: 2,
-            brand: 'TOYOTA',
-            model: 2022,
-            price: 45000,
-            color: 'black',
-            img: 'https://cdn.motor1.com/images/mgl/0eeJ2W/s1/2023-toyota-supra-with-manual-gearbox-europe.jpg',
-            conter:1
-    },
-        {
-            id: 3,
-                brand: 'BMW',
-                model: 2022,
-                price: 60000,
-                color: 'black',
-            img: 'https://cdn.motor1.com/images/mgl/nAX3Nk/s1/2022-bmw-m240i-exterior-review.jpg',
-            conter:1
-                
+        conter: 1
     },
     {
-          id: 4,
-              brand: 'PORSCHE',
-              model: 2022,
-              price: 100000,
-              color: 'black',
+        id: 2,
+        brand: 'TOYOTA',
+        model: 2022,
+        price: 45000,
+        color: 'black',
+        img: 'https://cdn.motor1.com/images/mgl/0eeJ2W/s1/2023-toyota-supra-with-manual-gearbox-europe.jpg',
+        conter: 1
+    },
+    {
+        id: 3,
+        brand: 'BMW',
+        model: 2022,
+        price: 60000,
+        color: 'black',
+        img: 'https://cdn.motor1.com/images/mgl/nAX3Nk/s1/2022-bmw-m240i-exterior-review.jpg',
+        conter: 1
+
+    },
+    {
+        id: 4,
+        brand: 'PORSCHE',
+        model: 2022,
+        price: 100000,
+        color: 'black',
         img: 'https://cdn.motor1.com/images/mgl/ZnnOg2/s1/singer-vehicle-design-turbo-study-side.jpg',
-              conter:1
+        conter: 1
     },
     {
-      id: 5,
-          brand: 'PEUGEOT',
-          model: 2022,
-          price: 90000,
-          color: 'black',
+        id: 5,
+        brand: 'PEUGEOT',
+        model: 2022,
+        price: 90000,
+        color: 'black',
         img: 'https://cdn.motor1.com/images/mgl/QeeNPB/s4/peugeot-408-live-pictures.webp',
-          conter:1
+        conter: 1
     },
-      {
-          id: 6,
-          brand: 'RENAULT',
-          model: 2022,
-          price: 90000,
-          color: 'black',
-          img: 'https://cdn.motor1.com/images/mgl/Mkkm8w/s1/renault-austral-at-2022-sofia-motor-show.jpg',
-          conter: 1
+    {
+        id: 6,
+        brand: 'RENAULT',
+        model: 2022,
+        price: 90000,
+        color: 'black',
+        img: 'https://cdn.motor1.com/images/mgl/Mkkm8w/s1/renault-austral-at-2022-sofia-motor-show.jpg',
+        conter: 1
     },
-       {
-           id:7,
-           brand: 'NISSAN',
-           model: 2022,
-           price: 90000,
-           color: 'black',
-           img: 'https://cdn.motor1.com/images/mgl/GBx91/s1/2014-476296-nissan-pulsar-nismo-rs-render1.jpg',
-           conter: 1
+    {
+        id: 7,
+        brand: 'NISSAN',
+        model: 2022,
+        price: 90000,
+        color: 'black',
+        img: 'https://cdn.motor1.com/images/mgl/GBx91/s1/2014-476296-nissan-pulsar-nismo-rs-render1.jpg',
+        conter: 1
     },
-        {
-            id: 8,
-            brand: 'FORD',
-            model: 2022,
-            price: 90000,
-            color: 'black',
-            img: 'https://cdn.motor1.com/images/mgl/BXXeYZ/s4/2022-ford-f-150-lightning.webp',
-            conter: 1
+    {
+        id: 8,
+        brand: 'FORD',
+        model: 2022,
+        price: 90000,
+        color: 'black',
+        img: 'https://cdn.motor1.com/images/mgl/BXXeYZ/s4/2022-ford-f-150-lightning.webp',
+        conter: 1
     },
-          {
-              id: 9,
-              brand: 'FORD',
-              model: 2022,
-              price: 90000,
-              color: 'black',
-              img: 'https://cdn.motor1.com/images/mgl/BXXeYZ/s4/2022-ford-f-150-lightning.webp',
-              conter: 1
+    {
+        id: 9,
+        brand: 'FORD',
+        model: 2022,
+        price: 90000,
+        color: 'black',
+        img: 'https://cdn.motor1.com/images/mgl/BXXeYZ/s4/2022-ford-f-150-lightning.webp',
+        conter: 1
     },
-           {
-               id: 10,
-               brand: 'HYUNDAI',
-               model: 2022,
-               price: 90000,
-               color: 'black',
-               img: 'https://cdn.motor1.com/images/mgl/nO3Zk/s4/2022-hyundai-sonata-n-line-night-edition-front-corner.webp',
-               conter: 1
+    {
+        id: 10,
+        brand: 'HYUNDAI',
+        model: 2022,
+        price: 90000,
+        color: 'black',
+        img: 'https://cdn.motor1.com/images/mgl/nO3Zk/s4/2022-hyundai-sonata-n-line-night-edition-front-corner.webp',
+        conter: 1
     },
-           {
-               id: 11,
-               brand: 'KIA',
-               model: 2022,
-               price: 90000,
-               color: 'black',
-               img: 'https://cdn.motor1.com/images/mgl/2NNArE/s1/2023-kia-soul.jpg',
-               conter: 1
-           },
-           {
-               id: 12,
-               brand: 'BENZ',
-               model: 2022,
-               price: 90000,
-               color: 'black',
-               img: 'https://cdn.motor1.com/images/mgl/jKLxo/s1/mercedes-b-class-2019.jpg',
-               conter: 1
-           },
+    {
+        id: 11,
+        brand: 'KIA',
+        model: 2022,
+        price: 90000,
+        color: 'black',
+        img: 'https://cdn.motor1.com/images/mgl/2NNArE/s1/2023-kia-soul.jpg',
+        conter: 1
+    },
+    {
+        id: 12,
+        brand: 'BENZ',
+        model: 2022,
+        price: 90000,
+        color: 'black',
+        img: 'https://cdn.motor1.com/images/mgl/jKLxo/s1/mercedes-b-class-2019.jpg',
+        conter: 1
+    },
+    {
+        id: 13,
+        brand: 'KIA',
+        model: 2022,
+        price: 90000,
+        color: 'gary',
+        img: 'https://cdn.motor1.com/images/mgl/jll1W7/s3/2022-kia-ev6.webp',
+        conter: 1
+    },
+    {
+        id: 14,
+        brand: 'HYUNDAI',
+        model: 2022,
+        price: 90000,
+        color: 'gary',
+        img: 'https://cdn.motor1.com/images/mgl/3WWenX/s3/hyundai-ioniq-6-shooting-brake-rendering.webp',
+        conter: 1
+    },
+    {
+        id: 15,
+        brand: 'PORSCHE',
+        model: 2022,
+        price: 90000,
+        color: 'gary',
+        img: 'https://cdn.motor1.com/images/mgl/BXXny6/s3/lamborghini-urus-phev-spy-photos.webp',
+        conter: 1
+    },
 ]
 export default data
