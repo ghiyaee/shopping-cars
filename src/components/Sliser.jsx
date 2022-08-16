91c9876de1d38b2e668c09720011b2bee88f1be8
ghiyaee <86221396+ghiyaee@users.noreply.github.com>
2022-08-16 17:17:03 +0430

    add style dot at slider

--- src/components/Sliser.jsx
+++ src/components/Sliser.jsx
@@ -3,9 +3,11 @@ import { useState, useEffect } from "react";
 import "../styles/slider.css";
 const Slider = ({ img }) => {
   let [imgCurrent, setImag] = useState(0);
+  let [dot, Setdot] = useState("color");
   const slide = {
     backgroundImage: ` url(${img[imgCurrent].img})`,
   };
+
   const goToPerv = () => {
     imgCurrent > 0 ? setImag(imgCurrent - 1) : setImag(img.length - 1);
   };
@@ -14,10 +16,6 @@ const Slider = ({ img }) => {
       ? setImag(imgCurrent + 1)
       : setImag((imgCurrent = 0));
   };
-  const dotHandel = (index) => {
-    setImag(index);
-  };
-
   useEffect(() => {
     const resetTime = setInterval(() => {
       goToNext();
@@ -30,13 +28,22 @@ const Slider = ({ img }) => {
     <>
       <div className="wrrap">
         <div className="arrow-left" onClick={goToPerv}>
-              {'<'}
+          {"<"}
         </div>
         <div className="arrow-right" onClick={goToNext}>
-              {'>'}
+          {">"}
         </div>
         <div className="slideStyle" style={slide}></div>
+
         <div className="dotStyle">
+          {img.map((item, ind) => (
+            <div
+              key={ind}
+              className={`dotSelf ${ind === imgCurrent ? dot : ""}`}
+            >
+              <i className="fa-solid fa-circle"></i>
+            </div>
+          ))}
         </div>
       </div>
     </>
